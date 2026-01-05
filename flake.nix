856805d8b96f96f6c7f3da1a6560ca02e2d4889e
y0usaf <74448287+y0usaf@users.noreply.github.com>
2026-01-05 11:58:26 -0500

    Add SSBU (Super Smash Bros Ultimate) xcursor theme
    - Convert Windows .cur cursor files to X11 xcursor format using win2xcur
    - Create SSBU theme directory with proper cursor symlinks for X11 standard names
    - Add ssbu-xcursor package to flake.nix
    - Update flake description to reflect multiple cursor theme support

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
-  description = "Deepin Dark Cursor Themes (X11 and Hyprland)";
+  description = "Cursor Themes Collection (X11 and Hyprland)";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+      in {
         packages = {
           deepin-dark-xcursor = pkgs.stdenv.mkDerivation {
             pname = "deepin-dark-xcursor";
@@ -19,19 +22,19 @@
             sourceRoot = ".";
 
             installPhase = ''
-              mkdir -p $out/share/icons/DeepinDarkV20-x11
-              if [ -d "$src/cursors" ]; then
-                cp -r $src/cursors $out/share/icons/DeepinDarkV20-x11/
-              fi
-              if [ -f "$src/index.theme" ]; then
-                cp $src/index.theme $out/share/icons/DeepinDarkV20-x11/
-              else
-                cat > $out/share/icons/DeepinDarkV20-x11/index.theme << EOF
-[Icon Theme]
-Name=DeepinDarkV20-x11
-Comment=Deepin Dark X11 Cursor Theme
-EOF
-              fi
+                            mkdir -p $out/share/icons/DeepinDarkV20-x11
+                            if [ -d "$src/cursors" ]; then
+                              cp -r $src/cursors $out/share/icons/DeepinDarkV20-x11/
+                            fi
+                            if [ -f "$src/index.theme" ]; then
+                              cp $src/index.theme $out/share/icons/DeepinDarkV20-x11/
+                            else
+                              cat > $out/share/icons/DeepinDarkV20-x11/index.theme << EOF
+              [Icon Theme]
+              Name=DeepinDarkV20-x11
+              Comment=Deepin Dark X11 Cursor Theme
+              EOF
+                            fi
             '';
 
             meta = {
@@ -68,6 +71,36 @@ EOF
 
             meta = {
               description = "Deepin Dark Hyprland cursor theme";
+              homepage = "https://github.com/y0usaf/Cursors";
+              license = pkgs.lib.licenses.mit;
+            };
+          };
+
+          ssbu-xcursor = pkgs.stdenv.mkDerivation {
+            pname = "ssbu-xcursor";
+            version = "1.0.0";
+            src = ./ssbu/xcursor;
+
+            sourceRoot = ".";
+
+            installPhase = ''
+              mkdir -p $out/share/icons/SSBU-x11
+              if [ -d "$src/cursors" ]; then
+                cp -r $src/cursors $out/share/icons/SSBU-x11/
+              fi
+              if [ -f "$src/index.theme" ]; then
+                cp $src/index.theme $out/share/icons/SSBU-x11/
+              else
+                cat > $out/share/icons/SSBU-x11/index.theme << EOF
+              [Icon Theme]
+              Name=SSBU-x11
+              Comment=Super Smash Bros Ultimate X11 Cursor Theme
+              EOF
+              fi
+            '';
+
+            meta = {
+              description = "Super Smash Bros Ultimate X11 cursor theme";
               homepage = "https://github.com/y0usaf/Cursors";
               license = pkgs.lib.licenses.mit;
             };
